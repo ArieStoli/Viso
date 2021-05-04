b805201d127e5a3c9da70b1270a805c4ae87199e
koreshroi <koresh.roi@gmail.com>
2021-05-04 15:35:25 +0300

    finish part 1

--- HW2/d3_308316371_315369181.js
+++ HW2/d3_308316371_315369181.js
@@ -1,89 +1,130 @@
-
-// let barChart = barChartDataset("task_2_output.csv");
-
-
-
-
-
-var a;
-var p2 = new Promise(function(resolve, reject) {
-    resolve();
-    reject();
-});
-p2.then(function(value) {
-    var a = barChartDataset("task_2_output.csv")
-    b = value["Regular"];
-
-    console.log(b);
-});
-
-
-
-
-
-
-// buildBarChart(barChart);
-
-// console.log(barChart);
-
-function barChartDataset(url) {
-    let retailPrice = [];
-    let dealerCost = [];
-    let types = [];
-    let barChartDataset = new Map();
-
-    d3.csv(url, function(error, data) {
-        for(let i = 0 ; i < data.length ; i++){
-            retailPrice.push(data[i]["Retail.Price"]);
-            dealerCost.push(data[i]["Dealer.Cost"]);
-            types.push(data[i]["Type"]);
-        }
-        for (let j = 0 ; j < types.length ; j++){
-            barChartDataset[types[j]] = [retailPrice[j],dealerCost[j]];
-        }
+const File_Pick = "task_2_output.csv";
+let barChartDataset = [];
+j = 0;
+d3.csv(File_Pick).then((data) =>{
+    // console.log(data);
+    data.forEach((d) => {
+        d["Dealer.Cost"] = +d["Dealer.Cost"];
+        d["Retail.Price"] = +d["Retail.Price"];
+        barChartDataset[j] = {"Type":d["Type"],
+                              "RetailPrice":d["Retail.Price"],
+                              "DealerCost":d["Dealer.Cost"]};
+        j = j +1 ;
+        return barChartDataset;
     });
-    console.log(barChartDataset);
-    return barChartDataset;
+    buildBarChart(barChartDataset);
+});
+
+function buildBarChart(dataset_clean) {
+    console.log(dataset_clean)
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    dataset = dataset_clean.map(i => {
+        i.Type = i.Type;
+        return i;
+    });
+
+    var container = d3.select('#d3id'),
+        width = 500,
+        height = 300,
+        margin = {top: 30, right: 20, bottom: 30, left: 50},
+        barPadding = .2,
+        axisTicks = {qty: 5, outerSize: 0, dateFormat: '%m-%d'};
+
+    var svg = container
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    var xScale0 = d3.scaleBand().range([0, width - margin.left - margin.right]).padding(barPadding);
+    var xScale1 = d3.scaleBand();
+    var yScale = d3.scaleLinear().range([height - margin.top - margin.bottom, 0]);
+
+    var xAxis = d3.axisBottom(xScale0).tickSizeOuter(axisTicks.outerSize);
+    var yAxis = d3.axisLeft(yScale).ticks(axisTicks.qty).tickSizeOuter(axisTicks.outerSize);
+
+    xScale0.domain(dataset_clean.map(d => d.Type));
+    xScale1.domain(['RetailPrice', 'DealerCost']).range([0, xScale0.bandwidth()]);
+    yScale.domain([0, d3.max(dataset_clean, d => d.RetailPrice > d.DealerCost ? d.RetailPrice : d.DealerCost)]);
+
+    var Type = svg.selectAll(".Type")
+        .data(dataset_clean)
+        .enter().append("g")
+        .attr("class", "Type")
+        .attr("transform", d => `translate(${xScale0(d.Type)},0)`);
+
+    /* Add RetailPrice bars */
+    Type.selectAll(".bar.RetailPrice")
+        .data(d => [d])
+        .enter()
+        .append("rect")
+        .style("fill","blue")
+        .attr("class", "bar RetailPrice")
+        .attr("x", d => xScale1('RetailPrice'))
+        .attr("y", d => yScale(d.RetailPrice))
+        .attr("width", xScale1.bandwidth())
+        .attr("height", d => {
+            return height - margin.top - margin.bottom - yScale(d.RetailPrice)
+        })
+        .on("mouseover", function(event,d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html("Retail Price<br/>" + d.RetailPrice.toFixed(3))
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
+
+    /* Add DealerCost bars */
+    Type.selectAll(".bar.DealerCost")
+        .data(d => [d])
+        .enter()
+        .append("rect")
+        .attr("class", "bar DealerCost")
+        .style("fill","red")
+        .attr("x", d => xScale1('DealerCost'))
+        .attr("y", d => yScale(d.DealerCost))
+        .attr("width", xScale1.bandwidth())
+        .attr("height", d => {
+            return height - margin.top - margin.bottom - yScale(d.DealerCost)
+        })
+        .on("mouseover", function(event,d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html("Dealer Cost<br/>" + d.DealerCost.toFixed(3))
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
+
+
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
+
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
 }
-
-
-
-
-
-// function buildBarChart(dataset) {
-//     var margin = {top: 20, right: 20, bottom: 30, left: 40},
-//         width = 960 - margin.left - margin.right,
-//         height = 500 - margin.top - margin.bottom;
-//
-//     var x0 = d3.scale.ordinal()
-//         .rangeRoundBands([0, width], .1);
-//
-//     var x1 = d3.scale.ordinal();
-//
-//     var y = d3.scale.linear()
-//         .range([height, 0]);
-//
-//     var color = d3.scale.ordinal()
-//         .range(["#C4F0FF", "#E8D1FF" , "#FFC4C4", "#F6FFC4"]);
-//
-//     var xAxis = d3.svg.axis()
-//         .scale(x0)
-//         .orient("bottom");
-//
-//     var yAxis = d3.svg.axis()
-//         .scale(y)
-//         .orient("left")
-//         .tickFormat(d3.format(".2s"));
-//
-//     var svg = d3.select("body").append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//
-//
-//
-//
-//     console.log(barChart["Regular"]);
-// }
