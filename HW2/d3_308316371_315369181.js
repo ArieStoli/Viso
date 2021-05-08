282152c0c7de001b8a333b294bbcecf40f2e83e5
Arie_Stoliarsky <arie.stoliarsky@gmail.com>
2021-05-08 11:23:00 +0300

    barchart done

--- HW2/d3_308316371_315369181.js
+++ HW2/d3_308316371_315369181.js
@@ -26,11 +26,12 @@ function buildBarChart(dataset_clean) {
         return i;
     });
 
-    var container = d3.select('#d3id'),
+    var container = d3.select('#barchart'),
         width = 500,
         height = 300,
-        margin = {top: 30, right: 20, bottom: 30, left: 50},
-        barPadding = .2,
+        svgPadding = 3;
+    var margin = {top: 30, right: 20, bottom: 50, left: 50},
+    barPadding = .2,
         axisTicks = {qty: 5, outerSize: 0, dateFormat: '%m-%d'};
 
     var svg = container
@@ -40,7 +41,7 @@ function buildBarChart(dataset_clean) {
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    var xScale0 = d3.scaleBand().range([0, width - margin.left - margin.right]).padding(barPadding);
+    var xScale0 = d3.scaleBand().range([margin.left, width - margin.left - margin.right]).padding(barPadding);
     var xScale1 = d3.scaleBand();
     var yScale = d3.scaleLinear().range([height - margin.top - margin.bottom, 0]);
 
@@ -77,11 +78,13 @@ function buildBarChart(dataset_clean) {
             div.html("Retail Price<br/>" + d.RetailPrice.toFixed(3))
                 .style("left", (event.pageX) + "px")
                 .style("top", (event.pageY - 28) + "px");
+            d3.select(this).style('stroke',"black").style("fill","darkblue");
         })
         .on("mouseout", function(d) {
             div.transition()
                 .duration(500)
                 .style("opacity", 0);
+            d3.select(this).style('stroke',"none").style("fill","blue");
         });
 
 
@@ -105,26 +108,65 @@ function buildBarChart(dataset_clean) {
             div.html("Dealer Cost<br/>" + d.DealerCost.toFixed(3))
                 .style("left", (event.pageX) + "px")
                 .style("top", (event.pageY - 28) + "px");
+            d3.select(this).style('stroke',"black").style("fill","darkred");
         })
         .on("mouseout", function(d) {
             div.transition()
                 .duration(500)
                 .style("opacity", 0);
+            d3.select(this).style('stroke',"none").style("fill","red");
         });
 
-
+    svg.append("text")
+        .attr("x", width/3)
+        .attr("y", svgPadding)
+        .attr("text-anchor", "right")
+        .style("font-size", "16px")
+        .text("Sales and costs per each vehicle category");
 
 
     // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
-
         .call(xAxis);
+
+    // X axis label
+        svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate(" + (width / 2.2) + "," + (height - (35)) + ")")
+
+        .text("Vehicle category");
 
     // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(" + (margin.left) + ",0)")
+
         .call(yAxis);
+
+    // Y axis label
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("x", - height/3)
+        .attr("y", 0)
+        .attr("transform", "rotate(-90)")
+        .text("USD - $");
+
+}
+
+function buildScatterPlot() {
+
+    let container = d3.select('#d3id'),
+        width = 500,
+        height = 300,
+        svgPadding = 3;
+
+    var svg = container
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
 }
