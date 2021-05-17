a6ed0e7c11f56efff7c379522e76fa45b2bd4495
aries <arie.stoliarsk@gmail.com>
2021-05-17 18:09:56 +0300

    done 4 real

--- HW2/d3_308316371_315369181.js
+++ HW2/d3_308316371_315369181.js
@@ -3,38 +3,36 @@ const carsCSV = 'cars.csv';
 const fuelPrices = 'Fuel_Prices.csv';
 
 const svgSize = {
-    width : 1200,
-    height : 450,
-    margin: {
-        left : 50,
-        right : 50,
-        top : 20,
-        bottom : 30
-    },
-    padding : 3,
-    barPadding : 0.2,
-}
+	width: 1200,
+	height: 450,
+	margin: {
+		left: 50,
+		right: 50,
+		top: 20,
+		bottom: 30,
+	},
+	padding: 3,
+	barPadding: 0.2,
+};
 
 const axisTicks = {
-    qty: 6,
-    outerSize: 0
-}
+	qty: 6,
+	outerSize: 0,
+};
 
 // buildBarChart();
 // buildScatterPlot();
 // buildLineChart();
 
 async function buildBarChart() {
-
-    let dataset = d3.csvParse(await getData(task2CSV), function (d) {
-        let temp = {
-                Type: d.Type,
-                RetailPrice: d['Retail.Price'],
-                DealerCost: d['Dealer.Cost'],
-        };
-        return temp;
-
-    });
+	let dataset = await d3.csv(task2CSV);
+	dataset = dataset.map((d) => {
+		return {
+			Type: d.Type,
+			RetailPrice: d['Retail.Price'],
+			DealerCost: d['Dealer.Cost'],
+		};
+	});
 
 	let div = d3
 		.select('body')
@@ -47,7 +45,7 @@ async function buildBarChart() {
 		return i;
 	});
 
-	let container = d3.select('.container')
+	let container = d3.select('.container');
 
 	let svg = container
 		.append('svg')
@@ -56,10 +54,15 @@ async function buildBarChart() {
 
 	let xScale0 = d3
 		.scaleBand()
-		.range([svgSize.margin.left, svgSize.width - svgSize.margin.left - svgSize.margin.right])
+		.range([
+			svgSize.margin.left,
+			svgSize.width - svgSize.margin.left - svgSize.margin.right,
+		])
 		.padding(svgSize.barPadding);
 	let xScale1 = d3.scaleBand();
-	let yScale = d3.scaleLinear().range([svgSize.height - svgSize.margin.top - svgSize.margin.bottom, 0]);
+	let yScale = d3
+		.scaleLinear()
+		.range([svgSize.height - svgSize.margin.top - svgSize.margin.bottom, 0]);
 
 	xScale0.domain(dataset_clean.map((d) => d.Type));
 	xScale1.domain(['RetailPrice', 'DealerCost']).range([0, xScale0.bandwidth()]);
@@ -70,11 +73,11 @@ async function buildBarChart() {
 		),
 	]);
 
-    let xAxis = d3.axisBottom(xScale0).tickSizeOuter(axisTicks.outerSize);
-    let yAxis = d3
-        .axisLeft(yScale)
-        .ticks(axisTicks.qty)
-        .tickSizeOuter(axisTicks.outerSize);
+	let xAxis = d3.axisBottom(xScale0).tickSizeOuter(axisTicks.outerSize);
+	let yAxis = d3
+		.axisLeft(yScale)
+		.ticks(axisTicks.qty)
+		.tickSizeOuter(axisTicks.outerSize);
 
 	let Type = svg
 		.selectAll('.Type')
@@ -95,7 +98,12 @@ async function buildBarChart() {
 		.attr('y', (d) => yScale(d.RetailPrice))
 		.attr('width', xScale1.bandwidth())
 		.attr('height', (d) => {
-			return svgSize.height - svgSize.margin.bottom - svgSize.margin.top - yScale(d.RetailPrice);
+			return (
+				svgSize.height -
+				svgSize.margin.bottom -
+				svgSize.margin.top -
+				yScale(d.RetailPrice)
+			);
 		})
 		.on('mouseover', function (event, d) {
 			div.transition().duration(200).style('opacity', 0.9);
@@ -121,7 +129,12 @@ async function buildBarChart() {
 		.attr('y', (d) => yScale(d.DealerCost))
 		.attr('width', xScale1.bandwidth())
 		.attr('height', (d) => {
-			return svgSize.height - svgSize.margin.bottom - svgSize.margin.top - yScale(d.DealerCost);
+			return (
+				svgSize.height -
+				svgSize.margin.bottom -
+				svgSize.margin.top -
+				yScale(d.DealerCost)
+			);
 		})
 		.on('mouseover', function (event, d) {
 			div.transition().duration(200).style('opacity', 0.9);
@@ -144,31 +157,39 @@ async function buildBarChart() {
 		.attr('text-anchor', 'right')
 		.style('font-size', '16px')
 		.text('Sales and costs per each vehicle category')
-		.attr("font-weight", 700)
-        .attr("class", "chartTitle");
+		.attr('font-weight', 700)
+		.attr('class', 'chartTitle');
 
 	// Add the X Axis
 	svg
 		.append('g')
 		.attr('class', 'x axis')
-		.attr('transform', `translate(0,${svgSize.height - svgSize.margin.bottom - svgSize.margin.top})`)
+		.attr(
+			'transform',
+			`translate(0,${
+				svgSize.height - svgSize.margin.bottom - svgSize.margin.top
+			})`
+		)
 		.call(xAxis);
 
 	// X axis label
 	svg
 		.append('text')
 		.attr('text-anchor', 'middle')
-		.attr('transform', 'translate(' + svgSize.width / 2.2 + ',' + (svgSize.height - 10) + ')')
+		.attr(
+			'transform',
+			'translate(' + svgSize.width / 2.2 + ',' + (svgSize.height - 10) + ')'
+		)
 		.text('Vehicle category')
-		.attr("font-weight", 700)
-        .attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	// Add the Y Axis
 	svg
 		.append('g')
 		.attr('class', 'y axis')
 		.attr('transform', 'translate(' + (svgSize.margin.left + 5) + ',0)')
-        .call(yAxis);
+		.call(yAxis);
 
 	// Y axis label
 	svg
@@ -178,40 +199,43 @@ async function buildBarChart() {
 		.attr('y', 10)
 		.attr('transform', 'rotate(-90)')
 		.text('USD - $')
-		.attr("font-weight", 700)
-        .attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	//legend
-	let legend = svg.append('g')
+	let legend = svg
+		.append('g')
 		.attr('class', 'legend')
 		.attr('transform', 'translate(' + (svgSize.margin.left + 12) + ', 0)');
 
-	legend.selectAll('rect')
+	legend
+		.selectAll('rect')
 		.data(['red', 'blue'])
 		.enter()
 		.append('rect')
 		.attr('x', 0)
-		.attr('y', function(d, i){
+		.attr('y', function (d, i) {
 			return i * 18;
 		})
 		.attr('width', 12)
 		.attr('height', 12)
-		.attr('fill', function(d){
+		.attr('fill', function (d) {
 			return d;
 		});
 
-	legend.selectAll('text')
+	legend
+		.selectAll('text')
 		.data(['RetailPrice', 'DealerCost'])
 		.enter()
 		.append('text')
-		.text(function(d){
+		.text(function (d) {
 			return d;
 		})
 		.attr('x', 18)
-		.attr('y', function(d, i){
+		.attr('y', function (d, i) {
 			return i * 18;
 		})
-		.attr("class", "legendText")
+		.attr('class', 'legendText')
 		.attr('text-anchor', 'start')
 		.attr('alignment-baseline', 'hanging');
 }
@@ -225,8 +249,8 @@ async function buildScatterPlot() {
 
 	let padding = 50;
 
-	let dataset = d3.csvParse(await getData(carsCSV), function (d) {
-
+	let dataset = await d3.csv(carsCSV);
+	dataset = dataset.map((d) => {
 		return {
 			cylinder: d['Cyl'],
 			hwyMPG: d['Hwy MPG'],
@@ -243,7 +267,7 @@ async function buildScatterPlot() {
 
 	let xScale = d3
 		.scaleLinear()
-		.domain([0, d3.max(dataset, (d) => parseInt(d.hp))+25])
+		.domain([0, d3.max(dataset, (d) => parseInt(d.hp)) + 25])
 		.range([padding, svgSize.width - padding * 2]);
 
 	let yScale = d3
@@ -256,9 +280,15 @@ async function buildScatterPlot() {
 		.domain([0, d3.max(dataset, (d) => parseInt(d.cylinder))])
 		.range([1.5, 5]);
 
-	let container = d3.select('.container').attr('width', svgSize.width).attr('height', svgSize.height);
+	let container = d3
+		.select('.container')
+		.attr('width', svgSize.width)
+		.attr('height', svgSize.height);
 
-	let svg = container.append('svg').attr('width', svgSize.width).attr('height', svgSize.height);
+	let svg = container
+		.append('svg')
+		.attr('width', svgSize.width)
+		.attr('height', svgSize.height);
 
 	svg
 		.selectAll('circle')
@@ -268,10 +298,10 @@ async function buildScatterPlot() {
 		.attr('cx', function (d) {
 			return xScale(d.hp);
 		})
-        .attr('cy', function (d) {
-            return yScale(d.hwyMPG);
-        })
-        .attr('r', function (d) {
+		.attr('cy', function (d) {
+			return yScale(d.hwyMPG);
+		})
+		.attr('r', function (d) {
 			return rScale(d.cylinder);
 		})
 		.attr('fill', function (d) {
@@ -309,10 +339,13 @@ async function buildScatterPlot() {
 	svg
 		.append('text')
 		.attr('text-anchor', 'middle')
-		.attr('transform', 'translate(' + svgSize.width / 2.2 + ',' + (svgSize.height - 16) + ')')
+		.attr(
+			'transform',
+			'translate(' + svgSize.width / 2.2 + ',' + (svgSize.height - 16) + ')'
+		)
 		.text('Horse power')
-		.attr("font-weight", 700)
-        .attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	// Add the Y Axis
 	svg
@@ -330,8 +363,8 @@ async function buildScatterPlot() {
 		.attr('y', 16)
 		.attr('transform', 'rotate(-90)')
 		.text('Highway Miles per Gallon')
-		.attr("font-weight", 700)
-        .attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	// title
 	svg
@@ -341,45 +374,97 @@ async function buildScatterPlot() {
 		.attr('text-anchor', 'right')
 		.style('font-size', '16px')
 		.text('Horse power to miles per gallon fuel consumption ratio')
-		.attr("font-weight", 700)
-        .attr("class", "chartTitle");
+		.attr('font-weight', 700)
+		.attr('class', 'chartTitle');
 
 	//legend
-	let legend = svg.append('g')
+	let legend = svg
+		.append('g')
 		.attr('class', 'legend')
-		.attr('transform', 'translate(' + (svgSize.margin.left + 12) + ',' + (svgSize.margin.top + 35) + ')');
+		.attr(
+			'transform',
+			'translate(' +
+			(svgSize.margin.left + 12) +
+			',' +
+			(svgSize.margin.top + 35) +
+			')'
+		);
 
-	legend.selectAll('rect')
-		.data(['red', 'black', 'grey'])
+	legend
+		.selectAll('circle')
+		.data(['red', 'black', 'grey', 3, 4, 5, 6, 8, 10, 12])
 		.enter()
 		.append('circle')
-		.attr('cx', 0)
-		.attr('cy', function(d, i){
-			return i * 18;
+		.attr('cx', function (d, i) {
+			if (typeof d == "string")
+				return 0;
+			else
+				return (10 + rScale(d)) * (i - 3);
 		})
-		.attr('r', 5)
-		.attr('fill', function(d){
-			return d;
-		});
+		.attr('cy', function (d, i) {
+			if (typeof d == "string")
+				return i * 18;
+			else
+				return 72;
+		})
+		.attr('r', function (d) {
+			if (typeof d == "string")
+				return 5;
+			else
+				return rScale(d);
+		})
+		.attr('fill', function (d) {
+			if (typeof d == "string")
+				return d;
+			else
+				return "white";
+		})
+		.attr("stroke", "black")
+		.attr('stroke-width', 1);
 
-	legend.selectAll('text')
-		.data(['Sports car', 'SUV', 'other'])
+
+	// legend
+	// 	.selectAll('circle')
+	// 	.data([3, 5, 7])
+	// 	.enter()
+	// 	.append('circle')
+	// 	.attr('cx', 0)
+	// 	.attr('cy', 72)
+	// 	.attr('r', function (d) {
+	// 		return d;
+	// 	})
+	// 	.attr('stroke', "black")
+	// 	.attr("fill", "white")
+	// 	.attr('stroke-width', 1);
+
+	legend
+		.selectAll('text')
+		.data(['Sports car', 'SUV', 'other', "size - cylinder:", 3, 5, 8, 12])
 		.enter()
 		.append('text')
-		.text(function(d){
+		.text(function (d) {
 			return d;
 		})
-		.attr('x', 18)
-		.attr('y', function(d, i){
-			return i * 18 - 5;
+		.attr('x', function (d, i) {
+			if (d == "size - cylinder:")
+				return 0;
+			else if (typeof d == "string")
+				return 18;
+			else
+				return (10 + rScale(d)) * (i - 4) * 2 - 5;
 		})
-		.attr("class", "legendText")
+		.attr('y', function (d, i) {
+			if (typeof d == "string")
+				return i * 18 - 5;
+			else
+				return 82;
+		})
+		.attr('class', 'legendText')
 		.attr('text-anchor', 'start')
 		.attr('alignment-baseline', 'hanging');
 }
 
 async function buildLineChart() {
-
 	let div = d3
 		.select('body')
 		.append('div')
@@ -387,8 +472,8 @@ async function buildLineChart() {
 		.style('opacity', 0);
 
 	let dates = [];
-	let dataset = d3.csvParse(await getData(fuelPrices), function (d) {
-
+	let dataset = await d3.csv(fuelPrices);
+	dataset = dataset.map((d) => {
 		dates.push(new Date(d3.timeParse('%m/%d/%Y')(d['Date'])));
 		return {
 			date: d3.timeParse('%m/%d/%Y')(d['Date']),
@@ -397,21 +482,13 @@ async function buildLineChart() {
 		};
 	});
 
-    let gasMin = d3.min(dataset, function (d) {
-        return d.priceGas;
-    });
+	let gasMax = d3.max(dataset, function (d) {
+		return d.priceGas;
+	});
 
-    let dieselMin = d3.min(dataset, function (d) {
-        return d.priceDiesel;
-    });
-
-    let gasMax = d3.max(dataset, function (d) {
-        return d.priceGas;
-    });
-
-    let dieselMax = d3.max(dataset, function (d) {
-        return d.priceDiesel;
-    });
+	let dieselMax = d3.max(dataset, function (d) {
+		return d.priceDiesel;
+	});
 
 	let container = d3
 		.select('.container')
@@ -425,21 +502,19 @@ async function buildLineChart() {
 
 	let xScale = d3.scaleTime().range([0, svgSize.width]);
 
-	let yScale = d3.scaleLinear().range([svgSize.height - svgSize.margin.bottom - svgSize.margin.top, 0]);
+	let yScale = d3
+		.scaleLinear()
+		.range([svgSize.height - svgSize.margin.bottom - svgSize.margin.top, 0]);
 
 	let color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    xScale.domain(
-        d3.extent(dates, function (d) {
-            return d;
-        })
-    );
+	xScale.domain(
+		d3.extent(dates, function (d) {
+			return d;
+		})
+	);
 
-    yScale.domain([
-        // dieselMin < gasMin ? gasMin - 5 : dieselMin - 5,
-        0,
-        dieselMax < gasMax ? gasMax + 15 : dieselMax + 15,
-    ]);
+	yScale.domain([0, dieselMax < gasMax ? gasMax + 15 : dieselMax + 15]);
 
 	let xAxis = d3.axisBottom(xScale).tickSizeOuter(axisTicks.outerSize);
 	let yAxis = d3
@@ -449,7 +524,6 @@ async function buildLineChart() {
 
 	color.domain(['Petrol', 'Diesel']);
 
-
 	var valueline = d3
 		.line()
 		.x(function (d) {
@@ -458,7 +532,6 @@ async function buildLineChart() {
 		.y(function (d) {
 			return yScale(d.priceGas);
 		});
-
 
 	var valueline2 = d3
 		.line()
@@ -473,17 +546,29 @@ async function buildLineChart() {
 	svg
 		.append('g')
 		.attr('class', 'x axis')
-		.attr('transform', `translate(${svgSize.margin.left},${svgSize.height - svgSize.margin.bottom - svgSize.margin.top})`)
+		.attr(
+			'transform',
+			`translate(${svgSize.margin.left},${
+				svgSize.height - svgSize.margin.bottom - svgSize.margin.top
+			})`
+		)
 		.call(xAxis);
 
 	// X axis label
 	svg
 		.append('text')
 		.attr('text-anchor', 'middle')
-		.attr('transform', 'translate(' + (svgSize.width - svgSize.margin.left) / 1.7 + ',' + (svgSize.height - svgSize.margin.bottom + svgSize.margin.top) + ')')
+		.attr(
+			'transform',
+			'translate(' +
+			(svgSize.width - svgSize.margin.left) / 1.7 +
+			',' +
+			(svgSize.height - svgSize.margin.bottom + svgSize.margin.top) +
+			')'
+		)
 		.text('Years')
-		.attr("font-weight", 700)
-		.attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	// Add the Y Axis
 	svg
@@ -500,8 +585,8 @@ async function buildLineChart() {
 		.attr('y', 16)
 		.attr('transform', 'rotate(-90)')
 		.text('USD - $')
-		.attr("font-weight", 700)
-        .attr("class", "axisLabel");
+		.attr('font-weight', 700)
+		.attr('class', 'axisLabel');
 
 	// title
 	svg
@@ -511,8 +596,8 @@ async function buildLineChart() {
 		.attr('text-anchor', 'right')
 		.style('font-size', '16px')
 		.text("Fuel prices from '04-20")
-		.attr("font-weight", 700)
-        .attr("class", "chartTitle");
+		.attr('font-weight', 700)
+		.attr('class', 'chartTitle');
 
 	svg
 		.append('path')
@@ -531,50 +616,46 @@ async function buildLineChart() {
 		.attr('transform', 'translate(' + svgSize.margin.left + ',0)');
 
 	//legend
-	let legend = svg.append('g')
+	let legend = svg
+		.append('g')
 		.attr('class', 'legend')
 		.attr('transform', 'translate(' + (svgSize.margin.left + 12) + ', 0)');
 
-	legend.selectAll('line')
+	legend
+		.selectAll('line')
 		.data(['red', 'blue'])
 		.enter()
 		.append('line')
 		.attr('x1', 18)
-		.attr('y1', function(d, i){
+		.attr('y1', function (d, i) {
 			return i * 18 + 5;
 		})
 		.attr('x2', 30)
 		.attr('y2', function (d, i) {
 			return i * 18 + 5;
 		})
-		.attr('style', function(d){
-			return "stroke-width:3; stroke:" + d + ";";
+		.attr('style', function (d) {
+			return 'stroke-width:3; stroke:' + d + ';';
 		});
 
-	legend.selectAll('text')
+	legend
+		.selectAll('text')
 		.data(['Petrol', 'Diesel'])
 		.enter()
 		.append('text')
-		.text(function(d){
+		.text(function (d) {
 			return d;
 		})
 		.attr('x', 36)
-		.attr('y', function(d, i){
+		.attr('y', function (d, i) {
 			return i * 18;
 		})
-		.attr("class", "legendText")
+		.attr('class', 'legendText')
 		.attr('text-anchor', 'start')
 		.attr('alignment-baseline', 'hanging');
 }
 
-async function getData(url) {
-	const response = await fetch(url);
-	const str = await response.text();
-	return str;
-}
-
 function getType(sp, su, wa, mi, pi) {
-	//TODO - ihsa
 	if (sp == 1) return 'Sports car';
 	else if (su == 1) return 'SUV';
 	else if (wa == 1) return 'Wagon';
@@ -594,27 +675,24 @@ function getColor(type) {
 	}
 }
 
-async function buttonClick(btn){
-
+async function buttonClick(btn) {
 	resetButtonColors();
 	let property = document.getElementById(btn);
 	property.style.backgroundColor = 'rgba(' + [139, 208, 133].join(',') + ')';
-	d3.selectAll("svg").remove();
-	if(btn=="toBar") {
+	d3.selectAll('svg').remove();
+	if (btn == 'toBar') {
 		await buildBarChart();
-	}
-	else if(btn=="toScatter") {
+	} else if (btn == 'toScatter') {
 		await buildScatterPlot();
-	}
-	else if(btn=="toLine") {
+	} else if (btn == 'toLine') {
 		await buildLineChart();
 	}
 }
 
 function resetButtonColors() {
 	let elements = document.getElementsByClassName('button'); // get all elements
-	for(let i = 0; i < elements.length; i++){
-		elements[i].style.backgroundColor =  'rgba(' + [255, 255, 255, 0.6].join(',') + ')';
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.backgroundColor =
+			'rgba(' + [255, 255, 255, 0.6].join(',') + ')';
 	}
 }
-
